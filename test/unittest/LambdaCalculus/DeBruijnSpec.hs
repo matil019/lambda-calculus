9e72ccb758897ea4d176b93ea0d594e36af01377
matil019 <yohashi1991@gmail.com>
2020-03-07 23:28:10 +0900

    Add a test case for (fromDeBruijn . toDebruijn)

--- test/unittest/LambdaCalculus/DeBruijnSpec.hs
+++ test/unittest/LambdaCalculus/DeBruijnSpec.hs
@@ -4,10 +4,16 @@ import LambdaCalculus.DeBruijn
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 
+import qualified LambdaCalculus.Term as Term
+
 spec :: Spec
 spec = do
-  it "toDeBruijn . fromDeBruijn == id" pending
+  it "toDeBruijn . fromDeBruijn == id" $
+    pendingWith "instance Arbitrary DeBruijn.Term not defined"
 
   -- an alternative to above until I define @instance Arbitrary DeBruijn.Term@
   prop "toDeBruijn . fromDeBruijn . toDeBruijn == toDeBruijn" $ \m ->
     (toDeBruijn . fromDeBruijn . toDeBruijn) m `shouldBe` toDeBruijn m
+
+  prop "fromDeBruijn (toDeBruijn m) `alphaEqv` m" $ \m ->
+    Term.unClosedTerm (fromDeBruijn (toDeBruijn m)) `shouldSatisfy` (`Term.alphaEqv` Term.unClosedTerm m)
