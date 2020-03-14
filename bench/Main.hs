aec0900dfc7a2b6823385d5e14ce9216e464bf78
matil019 <yohashi1991@gmail.com>
2020-03-14 23:48:58 +0900

    Add module Bench.Term
    so that Bench.DeBruijn can be added later

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,72 +1,18 @@
 module Main where
 
 import Control.Monad (replicateM)
-import Control.Monad.Trans.State.Strict (State, evalState)
 import Criterion.Main
-import Data.Conduit ((.|), runConduitPure)
-import Data.Maybe (catMaybes, mapMaybe)
-import Data.Traversable (for)
-import Numeric.Natural (Natural)
-import LambdaCalculus.Term
 
-import qualified Control.Monad.Trans.State.Strict as State
-import qualified Data.Conduit as C
-import qualified Data.Conduit.Combinators as C
+import qualified Bench.Term as Term
+import qualified LambdaCalculus.Term as Term
 import qualified Test.QuickCheck as Q
-
-reduceTerm :: Term -> Term
-reduceTerm m = runConduitPure
-   $ reduceSteps m
-  .| C.take 1000
-  .| C.takeWhile ((<= 1000) . countTerm)
-  .| C.lastDef m
-
-reduceTermS :: Term -> State Int Term
-reduceTermS m = do
-  remainingSteps <- State.get
-  let (result, n) = runConduitPure
-         $ reduceSteps m
-        .| C.take remainingSteps
-        .| C.takeWhile ((<= 1000) . countTerm)
-        .| C.getZipSink ((,) <$> C.ZipSink (C.lastDef m) <*> C.ZipSink C.length)
-  State.put (remainingSteps - n)
-  pure result
-
-apply :: (Natural, Natural) -> Term -> Term
-apply (a, b) m = App (App m (encodeChurchNumber a)) (encodeChurchNumber b)
-
-probs :: [(Natural, Natural)]
-probs =
-  [ (0, 0)
-  , (1, 0)
-  , (2, 1)
-  , (5, 3)
-  ]
-
--- | A naive implementation which reduces a term after applying numerals.
-reduce :: Term -> [Natural]
-reduce m = mapMaybe (interpretChurchNumber . reduceTerm . flip apply m) probs
-
--- | A two-fold implementation which reduces a term before and after applying numerals.
-reduce2 :: Term -> [Natural]
-reduce2 m = mapMaybe (interpretChurchNumber . reduceTerm . flip apply (reduceTerm m)) probs
-
--- | Like 'reduce2' but keeps the _total_ number of steps under a certain limit.
-reduce3 :: Term -> [Natural]
-reduce3 = flip evalState (length probs * 1000) . go
-  where
-  go m = do
-    m' <- reduceTermS m
-    fmap catMaybes $ for probs $ \prob -> do
-      m'' <- reduceTermS $ apply prob m'
-      pure $ interpretChurchNumber m''
 
 main :: IO ()
 main = defaultMain
-  [ env (Q.generate $ replicateM 100 $ unClosedTerm <$> Q.arbitrary) $ \terms ->
+  [ env (Q.generate $ replicateM 100 $ Term.unClosedTerm <$> Q.arbitrary) $ \terms ->
       bgroup "Term"
-      [ bench "reduce"  $ nf (map reduce)  terms
-      , bench "reduce2" $ nf (map reduce2) terms
-      , bench "reduce3" $ nf (map reduce3) terms
+      [ bench "reduce"  $ nf (map Term.reduce)  terms
+      , bench "reduce2" $ nf (map Term.reduce2) terms
+      , bench "reduce3" $ nf (map Term.reduce3) terms
       ]
   ]
