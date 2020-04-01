0f20ebc5910c6a46c7c1e9aa04eace30257d7352
matil019 <yohashi1991@gmail.com>
2020-04-01 13:39:58 +0900

    Add newtype FiniteList

--- src/LambdaCalculus/Utils.hs
+++ src/LambdaCalculus/Utils.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | Miscellaneous utilities.
 module LambdaCalculus.Utils where
+
+import Control.DeepSeq (NFData)
 
 -- | A safe '(!!)'.
 at :: Int -> [a] -> Maybe a
@@ -7,3 +10,9 @@ at i xs
   | i < 0 = Nothing
   | (x:_) <- drop i xs = Just x
   | otherwise = Nothing
+
+-- | A finite list.
+--
+-- Note that it is programmer's responsibility to make sure the list is indeed finite.
+newtype FiniteList a = FiniteList { unFiniteList :: [a] }
+ deriving (Applicative, Eq, Foldable, Functor, Monad, Monoid, NFData, Ord, Semigroup, Show)
