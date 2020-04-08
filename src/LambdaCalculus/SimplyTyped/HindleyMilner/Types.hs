eb5885069ad069f24992a7a5c10542c2e884d5b8
matil019 <yohashi1991@gmail.com>
2020-04-08 12:20:46 +0900

    Add derived instances

--- src/LambdaCalculus/SimplyTyped/HindleyMilner/Types.hs
+++ src/LambdaCalculus/SimplyTyped/HindleyMilner/Types.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module LambdaCalculus.SimplyTyped.HindleyMilner.Types where
+
+import Control.DeepSeq (NFData)
+import GHC.Generics (Generic)
 
 type VarType = String
 
@@ -7,20 +13,21 @@ data MonoType
   = VarType VarType
   | ConstType String
   | MonoType :-> MonoType
-  deriving (Eq, Show)
+  deriving (Eq, Generic, NFData, Show)
 infixr 1 :->
 
 data PolyType
   = Mono MonoType
   | ForAll VarType PolyType
+  deriving (Eq, Generic, NFData, Show)
 
 data Term
   = Var Int                -- ^ A variable (must start at @1@)
   | Abs Term               -- ^ An abstraction
   | App Term Term          -- ^ An application
   | Const MonoType String  -- ^ A constant
-  -- deriving (Eq, Generic, NFData, Show)
+  deriving (Eq, Generic, NFData, Show)
 
 newtype Subst = Subst [(VarType, MonoType)]
-  deriving (Monoid, Semigroup)
-  -- deriving (Eq, Monoid, Semigroup, Show)
+  deriving (Eq, Show)
+  deriving newtype (Monoid, NFData, Semigroup)
