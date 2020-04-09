e60399245bc20eb8c8aafe30ed8fbd09557c52eb
matil019 <yohashi1991@gmail.com>
2020-04-09 15:27:05 +0900

    More detailed benchmarks for index{,2}
    index2 is faster for iterating all elements (index/all/index2) than
    index but much slower than toList anyway.

--- bench/Main.hs
+++ bench/Main.hs
@@ -30,32 +30,42 @@ main = defaultMain
         [ bench "countTerm"  $ nf (map DeBruijn.countTerm)  terms
         , bench "countTerm2" $ nf (map DeBruijn.countTerm2) terms
         ]
-  , let gen = do
-          -- an arbitrary closed term and an arbitrary index in it
-          DeBruijn.ClosedTerm m <- Q.arbitrary
-          i <- Q.choose (0, DeBruijn.countTerm m - 1)
-          pure (m, i)
-    in
-    env (Q.generate $ replicateM 100 gen) $ \terms ->
-      bgroup "index"
-        [ bench "baseline" $ nf (map (\(term, _) -> Just term)) terms
-        , bgroup "midpoint" $
-            let mid m = DeBruijn.countTerm m `div` 2
+  , bgroup "index"
+      [ let gen = do
+              -- an arbitrary closed term and an arbitrary index in it
+              DeBruijn.ClosedTerm m <- Q.arbitrary
+              i <- Q.choose (0, DeBruijn.countTerm m - 1)
+              pure (m, i)
+        in
+        env (Q.generate $ replicateM 1000 $ Q.resize 200 gen) $ \terms ->
+          bgroup "individual"
+            [ bench "baseline" $ nf (map (\(term, _) -> Just term)) terms
+            , bgroup "midpoint" $
+                let mid m = DeBruijn.countTerm m `div` 2
+                in
+                [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  (mid term) term)) terms
+                , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 (mid term) term)) terms
+                ]
+            , bgroup "head"
+                [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  0 term)) terms
+                , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 0 term)) terms
+                ]
+            , bgroup "last"
+                [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  (DeBruijn.countTerm term - 1) term)) terms
+                , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 (DeBruijn.countTerm term - 1) term)) terms
+                ]
+            , bgroup "random"
+                [ bench "index"    $ nf (map (\(term, i) -> DeBruijn.index  i term)) terms
+                , bench "index2"   $ nf (map (\(term, i) -> DeBruijn.index2 i term)) terms
+                ]
+            ]
+      , env (fmap (map $ snd . DeBruijn.toDeBruijn mempty) $ Q.generate $ replicateM 100 $ Term.unClosedTerm <$> Q.arbitrary) $ \terms ->
+          bgroup "all" $
+            let allof f term = map (flip f term) $ [0..(DeBruijn.countTerm term - 1)]
             in
-            [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  (mid term) term)) terms
-            , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 (mid term) term)) terms
+            [ bench "toList (baseline)" $ nf (map DeBruijn.toList) terms
+            , bench "index"    $ nf (map $ allof DeBruijn.index) terms
+            , bench "index2"   $ nf (map $ allof DeBruijn.index2) terms
             ]
-        , bgroup "head"
-            [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  0 term)) terms
-            , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 0 term)) terms
-            ]
-        , bgroup "last"
-            [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  (DeBruijn.countTerm term - 1) term)) terms
-            , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 (DeBruijn.countTerm term - 1) term)) terms
-            ]
-        , bgroup "random"
-            [ bench "index"    $ nf (map (\(term, i) -> DeBruijn.index  i term)) terms
-            , bench "index2"   $ nf (map (\(term, i) -> DeBruijn.index2 i term)) terms
-            ]
-        ]
+      ]
   ]
