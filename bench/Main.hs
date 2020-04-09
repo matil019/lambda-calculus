16b0dcbdd861164e493c2d8bc3fc970a9e700118
matil019 <yohashi1991@gmail.com>
2020-04-09 15:05:27 +0900

    Compare the performance of index{,2}
    The original implementation index is faster in general case.
    However, index2 is much faster in querying the last element.
    (bench: index/last/index*)

--- bench/Main.hs
+++ bench/Main.hs
@@ -30,4 +30,32 @@ main = defaultMain
         [ bench "countTerm"  $ nf (map DeBruijn.countTerm)  terms
         , bench "countTerm2" $ nf (map DeBruijn.countTerm2) terms
         ]
+  , let gen = do
+          -- an arbitrary closed term and an arbitrary index in it
+          DeBruijn.ClosedTerm m <- Q.arbitrary
+          i <- Q.choose (0, DeBruijn.countTerm m - 1)
+          pure (m, i)
+    in
+    env (Q.generate $ replicateM 100 gen) $ \terms ->
+      bgroup "index"
+        [ bench "baseline" $ nf (map (\(term, _) -> Just term)) terms
+        , bgroup "midpoint" $
+            let mid m = DeBruijn.countTerm m `div` 2
+            in
+            [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  (mid term) term)) terms
+            , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 (mid term) term)) terms
+            ]
+        , bgroup "head"
+            [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  0 term)) terms
+            , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 0 term)) terms
+            ]
+        , bgroup "last"
+            [ bench "index"    $ nf (map (\(term, _) -> DeBruijn.index  (DeBruijn.countTerm term - 1) term)) terms
+            , bench "index2"   $ nf (map (\(term, _) -> DeBruijn.index2 (DeBruijn.countTerm term - 1) term)) terms
+            ]
+        , bgroup "random"
+            [ bench "index"    $ nf (map (\(term, i) -> DeBruijn.index  i term)) terms
+            , bench "index2"   $ nf (map (\(term, i) -> DeBruijn.index2 i term)) terms
+            ]
+        ]
   ]
