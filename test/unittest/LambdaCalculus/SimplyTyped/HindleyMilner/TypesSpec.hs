77cdaa6181b122f35223405cb3c4871c4b170997
matil019 <yohashi1991@gmail.com>
2020-04-26 01:12:12 +0900

    Use Fn (Fun) instead of Blind

--- test/unittest/LambdaCalculus/SimplyTyped/HindleyMilner/TypesSpec.hs
+++ test/unittest/LambdaCalculus/SimplyTyped/HindleyMilner/TypesSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module LambdaCalculus.SimplyTyped.HindleyMilner.TypesSpec where
@@ -8,20 +9,26 @@ import LambdaCalculus.SimplyTyped.HindleyMilner.Types
 import LambdaCalculus.SimplyTyped.HindleyMilner.Types.Instances ()
 import Test.Hspec
 import Test.Hspec.QuickCheck (modifyMaxSize, prop)
-import Test.QuickCheck (Arbitrary, Blind(Blind), CoArbitrary)
+import Test.QuickCheck (Arbitrary, CoArbitrary, Function, pattern Fn)
 
-traversalLawSpec :: (Eq s, Show s, Arbitrary s, Arbitrary a, CoArbitrary a) => Traversal' s a -> Spec
+traversalLawSpec
+  :: (Eq s, Show s, Arbitrary s, Show a, Arbitrary a, CoArbitrary a, Function a)
+  => Traversal' s a
+  -> Spec
 traversalLawSpec l = do
   -- we use Maybe as an example of Applicative
   prop "l pure == pure" $ \a ->
     l Just a `shouldBe` Just a
 
   -- we use [] as an example of Functor (because it's easiest to just write @map@)
-  modifyMaxSize (`div` 5) $
-    prop "fmap (l f) . l g == getCompose . l (Compose . fmap f . g)" $ \a (Blind (f :: a -> [a])) (Blind g) ->
+  modifyMaxSize (`div` 10) $
+    prop "fmap (l f) . l g == getCompose . l (Compose . fmap f . g)" $ \a (Fn (f :: a -> [a])) (Fn g) ->
       (map (l f) . l g) a `shouldBe` (getCompose . l (Compose . map f . g)) a
 
-lensLawSpec :: (Eq s, Show s, Arbitrary s, Eq a, Show a, Arbitrary a, CoArbitrary a) => Lens' s a -> Spec
+lensLawSpec
+  :: (Eq s, Show s, Arbitrary s, Eq a, Show a, Arbitrary a, CoArbitrary a, Function a)
+  => Lens' s a
+  -> Spec
 lensLawSpec l = do
   prop "view l (set l v s) == v" $ \v s ->
     view l (set l v s) `shouldBe` v
