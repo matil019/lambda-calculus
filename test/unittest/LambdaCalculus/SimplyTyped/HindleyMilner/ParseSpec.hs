aa34e082f9393ba0496d91edd6406a2e25d81631
matil019 <yohashi1991@gmail.com>
2020-04-26 15:52:41 +0900

    Add unit tests for formatTerm and parseTerm

--- test/unittest/LambdaCalculus/SimplyTyped/HindleyMilner/ParseSpec.hs
+++ test/unittest/LambdaCalculus/SimplyTyped/HindleyMilner/ParseSpec.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE LambdaCase #-}
 module LambdaCalculus.SimplyTyped.HindleyMilner.ParseSpec where
 
 import Control.Lens (transformM)
 import Data.Char (isUpper, toLower, toUpper)
 import LambdaCalculus.SimplyTyped.HindleyMilner.Parse
--- import LambdaCalculus.SimplyTyped.HindleyMilner.Term (formatTerm)
+import LambdaCalculus.SimplyTyped.HindleyMilner.Term (Term(Var, App, Const), formatTerm)
+import LambdaCalculus.SimplyTyped.HindleyMilner.Term.Instances ()
 import LambdaCalculus.SimplyTyped.HindleyMilner.Types (MonoType(VarType, ConstType), formatMonoType)
 import LambdaCalculus.SimplyTyped.HindleyMilner.Types.Instances ()
 import LambdaCalculus.Utils (isSimpleIdent)
@@ -34,10 +36,32 @@ instance Arbitrary ParsableMonoType where
     -- but I'm too lazy to implement an exact generator for it
     genApproxIdent = Q.listOf1 $ Q.chooseEnum ('0', 'z')
 
+newtype ParsableTerm = PT Term
+  deriving (Eq, Show)
+
+instance Arbitrary ParsableTerm where
+  arbitrary = do
+    m <- arbitrary
+    fmap PT $ transformPMT m
+    where
+    transformPMT = transformM $ \case
+      Const _ a -> arbitrary >>= \(PMT t) -> pure (Const t a)
+      m -> pure m
+
 spec :: Spec
 spec = do
-  -- prop "parseTerm . formatTerm == pure" $ \t ->
-  --   (parseTerm . formatTerm) t `shouldBe` Just t
+  prop "parseTerm . formatTerm == pure" $ \(PT m) ->
+    (parseTerm . formatTerm) m `shouldBe` Just m
+
+  describe "parsing Term applications" $ do
+    it "parseTerm \"1 2\"" $
+      parseTerm "1 2" `shouldBe` Just (App (Var 1) (Var 2))
+
+    it "parseTerm \"1 2 3\"" $
+      parseTerm "1 2 3" `shouldBe` Just (App (App (Var 1) (Var 2)) (Var 3))
+
+    it "parseTerm \"1 (2 3)\"" $
+      parseTerm "1 (2 3)" `shouldBe` Just (App (Var 1) (App (Var 2) (Var 3)))
 
   prop "parseMonoType . formatMonoType == pure" $ \(PMT t) ->
     (parseMonoType . formatMonoType) t `shouldBe` Just t
