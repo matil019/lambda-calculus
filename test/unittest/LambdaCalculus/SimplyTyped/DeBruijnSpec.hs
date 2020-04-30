054843a5a262c1246fa1cb9866728a9fa714ee64
matil019 <yohashi1991@gmail.com>
2020-04-30 14:32:45 +0900

    Add unit tests for interpretChurchNumber
    It can't interpret (\ 1). I tried the same test on the master branch at
    which the test succeeded.

--- test/unittest/LambdaCalculus/SimplyTyped/DeBruijnSpec.hs
+++ test/unittest/LambdaCalculus/SimplyTyped/DeBruijnSpec.hs
@@ -6,6 +6,7 @@ import LambdaCalculus.SimplyTyped.DeBruijn
 import LambdaCalculus.SimplyTyped.HindleyMilner.Term.Instances ()
 import LensLawSpec (prismLawSpec)
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck.Instances.Natural ()
 
 data SomeTypeSet
@@ -15,8 +16,27 @@ instance TypeSet SomeTypeSet where
 
 spec :: Spec
 spec = do
+  describe "interpretChurchNumber" $ do
+    it "(\\ \\ 1) == #0" $
+      interpretChurchNumber (Abs $ Abs $ Var 1) `shouldBe` Just 0
+
+    it "(\\ \\ 2 1) == #1" $
+      interpretChurchNumber (Abs $ Abs $ App (Var 2) (Var 1)) `shouldBe` Just 1
+
+    -- the special case; eta-reduced form
+    it "(\\ 1) == #1" $
+      interpretChurchNumber (Abs $ Var 1) `shouldBe` Just 1
+
+    it "(\\ \\ 2 (2 1)) == #2" $
+      interpretChurchNumber (Abs $ Abs $ App (Var 2) $ App (Var 2) (Var 1)) `shouldBe` Just 2
+
+    prop "interpretChurchNumber . encodeChurchNumber == Just" $ \n ->
+      (interpretChurchNumber . encodeChurchNumber) n `shouldBe` Just n
+
   describe "_closedTerm" $ do
-    prismLawSpec (_closedTerm :: Prism' Term (ClosedTerm SomeTypeSet))
+    describe "should follow the prism law" $
+      prismLawSpec (_closedTerm :: Prism' Term (ClosedTerm SomeTypeSet))
 
   describe "_churchNumber" $ do
-    prismLawSpec _churchNumber
+    describe "should follow the prism law" $
+      prismLawSpec _churchNumber
