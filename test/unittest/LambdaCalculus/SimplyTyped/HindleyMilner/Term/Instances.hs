aa0ccbcaf841affc2a118a81d98613a3f9c67989
matil019 <yohashi1991@gmail.com>
2020-05-05 15:42:11 +0900

    Add a unit test to make sure the new reduceBeta is correct
    The new one is tested to be equivalent with the old one.

--- test/unittest/LambdaCalculus/SimplyTyped/HindleyMilner/Term/Instances.hs
+++ test/unittest/LambdaCalculus/SimplyTyped/HindleyMilner/Term/Instances.hs
@@ -3,7 +3,7 @@ module LambdaCalculus.SimplyTyped.HindleyMilner.Term.Instances where
 
 import LambdaCalculus.SimplyTyped.HindleyMilner.Types.Instances ()
 import LambdaCalculus.SimplyTyped.DeBruijn
-import Test.QuickCheck (Arbitrary, arbitrary)
+import Test.QuickCheck (Arbitrary, arbitrary, genericShrink, shrink)
 
 import qualified Test.QuickCheck as Q
 
@@ -13,7 +13,14 @@ instance Arbitrary Term where
     Q.NonNegative (Q.Small freeNum) <- arbitrary
     genTerm constants freeNum
 
+  shrink = filter positiveVar . genericShrink
+    where
+    positiveVar (Var x) = x > 0
+    positiveVar _ = True
+
 instance Arbitrary ClosedTerm where
   arbitrary = do
     let constants = fmap Just arbitrary
     genClosedTerm constants
+
+  shrink = map ClosedTerm . shrink . unClosedTerm
